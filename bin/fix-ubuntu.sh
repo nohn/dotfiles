150b09c3fb34624054bbf928a86a7ba0f57a246f
Sebastian Nohn <sebastian@nohn.net>
2020-07-31 09:04:51 +0200

    move gists to setup script

--- bin/fix-ubuntu.sh
+++ bin/fix-ubuntu.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+# Fix DNS
+sudo sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
+sudo sh -c 'rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf'
+sudo systemctl restart systemd-resolved
+# (Un)fix secondary screen in multi-head setups
+gsettings set org.gnome.shell.overrides workspaces-only-on-primary false
 # Get rid of Evolution as much as possible
-
 cd /usr/share/dbus-1/services
 sudo cp org.gnome.evolution.dataserver.AddressBook.service org.gnome.evolution.dataserver.AddressBook.service.backup
 sudo cp org.gnome.evolution.dataserver.Calendar.service org.gnome.evolution.dataserver.Calendar.service.backup
